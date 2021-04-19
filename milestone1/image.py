2b1791fdfa91725cd0922c909e1e68b04fa6894c
Beatriz Silva <beatriz.dias.silva98@ua.pt>
2021-04-19 12:11:03 +0100

    Resize of image

--- milestone1/image.py
+++ milestone1/image.py
@@ -3,7 +3,14 @@ import sys
 img = cv.imread(cv.samples.findFile("groundtruth-rot0-2.png"))
 if img is None:
     sys.exit("Could not read the image.")
+
+scale_percent = 20 # percent of original size
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+img = cv.resize(img, dim)
+
 cv.imshow("Display window", img)
 k = cv.waitKey(0)
-if k == ord("s"):
-    cv.imwrite("groundtruth-rot0-2.png", img)
+cv.destroyAllWindows()
