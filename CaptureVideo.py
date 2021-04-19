87a6ec1e3ad5a560f272d03451aa46b64d5becb2
lavradorafonso <80522766+lavradorafonso@users.noreply.github.com>
2021-04-19 10:01:07 +0100

    Update CaptureVideo.py

--- CaptureVideo.py
+++ CaptureVideo.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2 as cv
 cap = cv.VideoCapture(0)
+
+out = cv.VideoWriter("TestCaptureVideo.avi",cv.VideoWriter_fourcc(*"MJPG"), 30,(640,480))
+
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -14,12 +17,16 @@ while True:
     # Our operations on the frame come here
     #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame = cv.flip(frame, 1)
+
+    # write the flipped frame
+    out.write(frame)
+
     # Display the resulting frame
     cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
 cap.release()
+out.release()
 cv.destroyAllWindows()
-
 
