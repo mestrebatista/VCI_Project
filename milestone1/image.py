4cae1da9ed7dba5a80e2f6c1cac15af628e17737
Beatriz Silva <beatriz.dias.silva98@ua.pt>
2021-04-25 23:10:17 +0100

    function imageResize

--- milestone1/image.py
+++ milestone1/image.py
@@ -1,34 +1,33 @@
+# Image resizing program
 import cv2 as cv
 import sys
-import numpy as np
-from matplotlib import pyplot as plt
+
+def imageResize(image):
+
+    # Percent of original size
+    scale_percent = 20
+
+    # Width
+    width = int(image.shape[1] * scale_percent / 100)
+    # Height
+    height = int(image.shape[0] * scale_percent / 100)
+
+    # Dimensions
+    dim = (width, height)
+
+    # Resizes the image with the new dimensions
+    resizedImage = cv.resize(image, dim)
+
+    # Returns the image resized
+    return resizedImage
+
 
 img = cv.imread(cv.samples.findFile("groundtruth-rot0-2.png"))
 if img is None:
     sys.exit("Could not read the image.")
 
-# Using resize to decrease the size of the image
-scale_percent = 20 # percent of original size
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dim = (width, height)
-img = cv.resize(img, dim)
+img = imageResize(img)
 
-hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-
-# define range of color in HSV
-lower = np.array([0,0,255])
-upper= np.array([255,255,255])
-
-# Threshold the HSV image to get only blue colors
-mask = cv.inRange(hsv, lower, upper)
-
-# Bitwise-AND mask and original image
-res = cv.bitwise_and(img,img, mask= mask)
-
-plt.subplot(131),plt.imshow(img),plt.title('image')
-plt.subplot(132),plt.imshow(mask),plt.title('mask')
-plt.subplot(133),plt.imshow(res),plt.title('res')
-
-plt.show()
+cv.imshow('image',img)
+k = cv.waitKey(0) & 0xFF
 
