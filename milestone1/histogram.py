f74bbd35e89a820a40d0cd6fd84704949a4248ad
mestrebatista <jose.mestrebatista@gmail.com>
2021-04-26 11:30:58 +0100

    video capture image done, original and hsv histogram done

--- milestone1/histogram.py
+++ milestone1/histogram.py
@@ -3,16 +3,16 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 
 
-# alterar codigo para suportar video input!!
-img = cv.imread('NewPicture.jpg')
+img = cv.imread('NewPicture.jpg') #adquire a imagem do vídeo
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)# img to gray
-cv.imshow('image1',gray)
+#tratamento da imagem original
+cv.imshow('image1',img)
 cv.namedWindow('image1', cv.WINDOW_AUTOSIZE)
-grey_hist=cv.calcHist([gray.ravel()],[0],None,[256],[0,256])
-plt.plot(grey_hist)
+origin_hist=cv.calcHist([img],[0],None,[256],[0,256])
+plt.plot(origin_hist)
 plt.show()
 
+# tratamento da imagem para escala HSV
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)  # img to HSV
 cv.imshow('image2',hsv)
 cv.namedWindow('image2', cv.WINDOW_NORMAL)
@@ -20,12 +20,12 @@ hsv_hist=cv.calcHist([hsv.ravel()],[0],None,[256],[0,256])
 plt.plot(hsv_hist)
 plt.show()
 
-#pode nao ser necessário visto que vamos usar o hsv
-yuv = cv.cvtColor(img, cv.COLOR_BGR2YUV)  # img to YUV
-cv.imshow('image3',yuv)
-cv.namedWindow('image3', cv.WINDOW_NORMAL)
-yuv_hist=cv.calcHist([yuv.ravel()],[0],None,[256],[0,256])
-plt.plot(yuv_hist)
+# tratamento da imagem para escala cinzenta
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)# img to gray
+cv.imshow('image1',gray)
+cv.namedWindow('image1', cv.WINDOW_AUTOSIZE)
+grey_hist=cv.calcHist([gray.ravel()],[0],None,[256],[0,256])
+plt.plot(grey_hist)
 plt.show()
 
 cv.waitKey(0)
