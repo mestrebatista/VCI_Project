b155a44a7be1068c0b704ab0e21a04071db38701
mestrebatista <jose.mestrebatista@gmail.com>
2021-05-02 10:41:50 +0100

    equalization update

--- milestone1/hist_equalization.py
+++ milestone1/hist_equalization.py
@@ -1,18 +1,16 @@
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
+
 
 def hist_eq(picture):
-    img = cv.imread(picture)
+    img = cv.imread(picture,0)
 
     #CLAHE object
     clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     cl1 = clahe.apply(img)
     #cl1 = cv.equalizeHist(img)
-    cv.imwrite('clahe_2.png',cl1)
+    cv.imwrite('Better_Image.png',cl1)
 
-    #cap.release()
-    cv.destroyAllWindows()
 hist_eq("legoImage.png")
 
 
