5a3dac5f5b98d5952b85dddc56e76373215436e5
mestrebatista <jose.mestrebatista@gmail.com>
2021-05-14 16:01:42 +0100

    added lego_detect and contours files

---
+++ milestone2/Contours.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import numpy as np
+
+
+def getContours(img, imgCanny):
+    contours, hierarchy = cv.findContours(imgCanny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    imgCopy = img.copy()
+    for cnt in contours:
+        area = cv.contourArea(cnt + 1)
+        print(area)  # area
+        if area < 7900:
+            imgContour = cv.drawContours(imgCopy, cnt, -1, (0, 255, 0), 2)
+            perimetro = cv.arcLength(cnt, True)
+            print(perimetro)  # perimetro
+            cornerPoints = cv.approxPolyDP(cnt, 0.02 * perimetro, True)
+            numCorners = len(cornerPoints)
+            print(numCorners)  # numero de vértices
+            x, y, w, h = cv.boundingRect(cornerPoints)
+
+            if numCorners == 4:
+                aspRatio=w/float(h)
+                if aspRatio>0.30 and aspRatio<1.70:
+                    LegoType="Pequeno"
+                elif aspRatio>0 and aspRatio<2:
+                    LegoType="Medio"
+                else:
+                    LegoType="Grande"
+            else: LegoType = "None"
+
+            cv.rectangle(imgCopy,(x,y),(x+w,y+h),(255,0,0),2)
+            cv.putText(imgCopy,LegoType,(x+(w//4)-30,y+(h)+20),cv.FONT_HERSHEY_SIMPLEX,0.7,(255,255,255),1)
+
+    return imgContour
---
+++ milestone2/lego_detect.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import numpy as np
+import function
+import Contours
+
+img=cv.imread("groundtruth-rot0-2.png")
+img = function.imageResize(img) #resize the original image
+
+#convert image: blur it
+#blur the image for better edge detection
+imgBlur=cv.GaussianBlur(img,(7,7),0)
+
+#find edges in the images using Canny
+imgCanny=cv.Canny(imgBlur,50,50)
+#get contours of the edges in the image
+imgContour=Contours.getContours(img,imgCanny)
+
+
+#show results
+#cv.imshow("original",img)
+#cv.imshow("Blur",imgBlur)
+cv.imshow("Edges",imgCanny)
+cv.imshow("Contours",imgContour)
+cv.waitKey(0)
