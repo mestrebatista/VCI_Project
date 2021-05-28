78c44ce7ed5b601916cbbb0d1e4d0817dd5416e4
mestrebatista <jose.mestrebatista@gmail.com>
2021-05-28 17:13:28 +0100

    func update

--- milestone2/CameraCalibration/camera_calibration.py
+++ milestone2/CameraCalibration/camera_calibration.py
@@ -3,80 +3,85 @@ import cv2 as cv
 import glob
 
 ### encontrar os cantos do plano de xadrez ###
+def calib():
+    xadrez_dim=(6,6)
+    #frameSize=(1440,1080)
+    frameSize=(640,480)
 
-xadrez_dim=(6,6)
-#frameSize=(1440,1080)
-frameSize=(640,480)
+    # termination criteria
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((xadrez_dim[0]*xadrez_dim[1],3), np.float32)
+    objp[:,:2] = np.mgrid[0:xadrez_dim[0],0:xadrez_dim[1]].T.reshape(-1,2)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((xadrez_dim[0]*xadrez_dim[1],3), np.float32)
-objp[:,:2] = np.mgrid[0:xadrez_dim[0],0:xadrez_dim[1]].T.reshape(-1,2)
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    images = glob.glob('*.jpg')
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-images = glob.glob('*.jpg')
+    for fname in images:
+        img = cv.imread(fname)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-for fname in images:
-    img = cv.imread(fname)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv.findChessboardCorners(gray, xadrez_dim, None)
 
-    # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, xadrez_dim, None)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+            corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+            imgpoints.append(corners)
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners)
+            # Draw and display the corners
+            cv.drawChessboardCorners(img, xadrez_dim, corners2, ret)
+            cv.imshow('img', img)
+            cv.waitKey(1000)
 
-        # Draw and display the corners
-        cv.drawChessboardCorners(img, xadrez_dim, corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(1000)
+    cv.destroyAllWindows()
 
-cv.destroyAllWindows()
+    # Calibração #
+    # meter frameSize se necessário
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
 
-# Calibração #
-# meter frameSize se necessário
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
+    print("Camera calibrada:", ret)
+    print("\nMatriz da Camara:\n", mtx)
+    #print("\nParametros de distorção:\n", dist)
+    #print("\nVetores de Rotação:\n", rvecs)
+    #print("\nVetores de Translação:\n", tvecs)
 
-print("Camera calibrada:", ret)
-print("\nMatriz da Camara:\n", mtx)
-print("\nParametros de distorção:\n", dist)
-print("\nVetores de Rotação:\n", rvecs)
-print("\nVetores de Translação:\n", tvecs)
+    ### Undistortion ###
 
-### Undistortion ###
+    #alterar leitura de imagem final
+    img = cv.imread('Picture15.jpg')
+    h,  w = img.shape[:2]
+    newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
-#alterar leitura de imagem final
-img = cv.imread('Picture15.jpg')
-h,  w = img.shape[:2]
-newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+    # undistort
+    #dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+    # crop the image
+    #x, y, w, h = roi
+    #dst = dst[y:y+h, x:x+w]
+    #cv.imwrite('calib_Result1.png', dst)
 
-# undistort
-dst = cv.undistort(img, mtx, dist, None, newcameramtx)
-# crop the image
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
-cv.imwrite('calib_Result1.png', dst)
+    # undistort with remapping
+    mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+    dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+    print("\nUndistorted Matrix:\n", dst)
+    cv.imwrite('calib_Result2.png', dst)
 
-# undistort with remapping
-mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
-dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
-# crop the image
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
-cv.imwrite('calib_Result2.png', dst)
+    ### Erro de Reprojeção ###
 
-### Erro de Reprojeção ###
+    mean_error = 0
+    for i in range(len(objpoints)):
+        imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+        error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+        mean_error += error
+    print( "\ntotal error: {}".format(mean_error/len(objpoints)) )
 
-mean_error = 0
-for i in range(len(objpoints)):
-    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
-    mean_error += error
-print( "\ntotal error: {}".format(mean_error/len(objpoints)) )
+    return newcameramtx
+
+newcameramatrix=calib()
