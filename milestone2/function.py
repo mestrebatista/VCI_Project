4612597135efce8f7af0b200eb8d72782d16c7e2
Beatriz Silva <beatriz.dias.silva98@ua.pt>
2021-05-28 17:33:42 +0100

    Actualização

--- milestone2/function.py
+++ milestone2/function.py
@@ -3,8 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 import Measure
 
-def contour(image):
-    #img = cv.imread(image)
+def contour1(image):
+    #image = cv.imread(img)
     imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(imgray, 0, 255, 0)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -19,31 +19,42 @@ def contour(image):
     return image,contours
 
 
+def contour2(image, imgray):
+    ret, thresh = cv.threshold(imgray, 0, 255, 0)
+    contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cv.drawContours(image, contours, -1, (0,255,0), 6)
+
+    cv.imshow('Display', image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    return image,contours
+
+
 def colorDetection(img):
 
     # Convert BGR to HSV
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
     # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    lower_blue = np.array([112, 220, 50])
+    upper_blue = np.array([120, 255, 255])
 
-    # define range of yellow color in HSV
-    lower_yellow = np.array([20, 100, 100])
-    upper_yellow = np.array([30, 255, 255])
+    # define range of green color in HSV
+    lower_green = np.array([50, 100, 100])
+    upper_green = np.array([105, 255, 255])
 
     # define range of red color in HSV
-    lower_red = np.array([0, 50, 120])
-    upper_red = np.array([10, 255, 255])
+    lower_red = np.array([150, 155, 0])
+    upper_red = np.array([179, 255, 255])
 
-    # Threshold the HSV image
+    # Threshold the HSV image to get only blue colors
     mask1 = cv.inRange(hsv, lower_blue, upper_blue)
-    mask2 = cv.inRange(hsv, lower_yellow, upper_yellow)
+    mask2 = cv.inRange(hsv, lower_green, upper_green)
     mask3 = cv.inRange(hsv, lower_red, upper_red)
-    mask = mask1+mask2+mask3
+    mask = mask2     # + mask2 + mask3;
 
     # Bitwise-AND mask and original image
-    res = cv.bitwise_and(img, img, mask= mask)
+    res = cv.bitwise_and(img, img, mask=mask)
 
     # Changing Colors to RGB in  order to do the plot
     img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
@@ -51,10 +62,11 @@ def colorDetection(img):
     mask = cv.cvtColor(mask, cv.COLOR_BGR2RGB)
 
     res = imageResize(res)
+    #cv.imwrite("img_new.jpg", res)
 
-    plt.subplot(1,3,1), plt.imshow(img), plt.title('Original Image')
-    plt.subplot(1,3,2), plt.imshow(res), plt.title('Range of Blue, Green and RED')
-    plt.subplot(1,3,3), plt.imshow(mask), plt.title('Mask')
+    plt.subplot(1, 3, 1), plt.imshow(img), plt.title('Original Image')
+    plt.subplot(1, 3, 2), plt.imshow(res), plt.title('Range of Blue, Green and RED')
+    plt.subplot(1, 3, 3), plt.imshow(mask), plt.title('Mask')
     plt.show()
 
     return res
@@ -78,10 +90,37 @@ def imageResize(image):
     # Returns the image resized
     return resizedImage
 
-img = cv.imread("legoPictures/Legos1.jpg")
+
+def backroundSub(background, img):
+
+    fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
+
+    fgmask = fgbg.apply(background)
+
+
+    fgmask = fgbg.apply(img)
+
+    edges = cv.Canny(fgmask, 100, 200)
+
+    cv.imshow('edges',edges)
+    k = cv.waitKey(0) & 0xff
+
+    return edges
+
+
+img = cv.imread("legoPictures/Legos10.jpg")
+background = cv.imread("legoPictures/Background.jpg")
 img = imageResize(img)
+edges = backroundSub(background, img)
+img, contours = contour2(img, edges)
+
+color = colorDetection(img)
+#cont = contour1(color)
+
+""""
 img = colorDetection(img)
 img,contours=contour(img)
 img=Measure.getMeasure(img, contours)
+"""
 
 
